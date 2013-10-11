7a8f2f5b000640fb16277533d5c76fb7eff90454
LEETAEHO <tae1560@gmail.com>
2013-10-11 15:55:11 +0900

    initial commit

---
+++ lib/gumbly/framework.rb
@@ -0,0 +1,9 @@
+require "gumbly/framework/version"
+
+module Gumbly
+  module Framework
+    # Your code goes here...
+	class Engine < ::Rails::Engine
+	end
+  end
+end
---
+++ lib/gumbly/framework/version.rb
@@ -0,0 +1,5 @@
+module Gumbly
+  module Framework
+    VERSION = "0.0.1"
+  end
+end
